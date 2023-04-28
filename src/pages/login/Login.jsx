a11af39610436d39fba35fdd8d6d9d6de56416df
rafi <r.rafi1615005@gmail.com>
2023-04-28 07:39:26 +0600

    sign-in

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,7 @@
 import "./login.scss"
 import { signInPopup } from '../../utils/firebase/firebase.utils';
+import GoogleIcon from '@mui/icons-material/Google';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Login = () => {
   const logGoogleUser = async () => { 
@@ -7,9 +9,33 @@ const Login = () => {
     console.log(response)
   }
   return (
-    <div>
-      <button onClick={logGoogleUser}>Sign In with Google</button>
-      <form></form>
+    <div className="login">
+      {/* <button onClick={logGoogleUser}>Sign In with Google</button> */}
+      <div className="formWrapper">
+        
+        <h1 style={{textAlign:"center"}}>Login</h1>
+        <form className="loginForm">
+          <label className="labelStyle">Name</label>
+          <input className="inputStyle" placeholder={` Name...`}/>
+
+          <label className="labelStyle">Email</label>
+          <input className="inputStyle" placeholder=" Email..."/>
+
+          <label className="labelStyle">Password</label>
+          <input className="inputStyle" placeholder=" Password..."/>
+          <p className="forget">forgot password?</p>
+          <button className="loginButton">Login</button>
+        </form>
+  
+       <p className="signText">Or SignUp using</p>
+        <div className="icons">
+          <GoogleIcon className="icon"/>
+          <TwitterIcon className="icon"/>
+        </div>
+       <p className="signText">don't have an account? sign up!</p>
+      </div>
+
+
     </div>
   )
 }
